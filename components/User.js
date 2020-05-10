3ab43a4f5ea466f64c985d7245d2044a2345a885
vtrrix <viveksonybcd@gmail.com>
2020-05-10 17:23:58 +0530

    added fetch api (issues present)

--- components/User.js
+++ components/User.js
@@ -1,7 +1,23 @@
-var User = new Vue({
-	el: "#user",
-	data: {
+let User = new Vue({
+	el: "#User",
+	data: () => ({
 		name: "Vtrrix",
-		imgSrc: "",
+		userData: [],
+	}),
+	methods: {
+		fetchData: (name) => {
+			fetch(`https://api.github.com/users/${name}`)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+				})
+				.catch((err) => {
+					console.log(err);
+					alert("Sorry unable to get data");
+				});
+		},
+	},
+	created() {
+		this.fetchData(this.name);
 	},
 });
